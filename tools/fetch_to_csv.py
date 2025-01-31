8f241975afc732ed9385e0d623a91f3fb6d0b8b9
dae-ne <rafalcz29@gmail.com>
2025-01-31 01:36:18 +0100

    Update tool for fetching historical data

--- tools/fetch_to_csv.py
+++ tools/fetch_to_csv.py
@@ -1,37 +1,47 @@
-import requests
 import csv
+import os
 import time
+from datetime import datetime, timedelta
 
-URL = 'https://developers.lotto.pl/api/open/v1/lotteries/draw-results/by-date-per-game'
-HEADERS = {
-    'Content-Type': 'application/json',
-    'secret': ''
-}
+import requests
+from dotenv import load_dotenv
+
+
+ENDPOINT = 'https://developers.lotto.pl/api/open/v1/lotteries/draw-results/by-date-per-game'
 OUTPUT_FILE = 'data.csv'
 
 
-def fetch_draw_results(date):
+def fetch_draw_results(date, api_key):
     try:
+        headers = {
+            'User-Agent': 'rczajka.me',
+            'secret': api_key
+        }
         params = {
             'gameType': 'Lotto',
             'drawDate': date,
             'index': '1',
             'size': '2',
-            'sort': 'drawBySystemId',
+            'sort': 'drawSystemId',
             'order': 'ASC'
         }
-        response = requests.get(URL, params=params, headers=HEADERS)
+        response = requests.get(ENDPOINT, headers=headers, params=params)
+
+        if response.status_code == 404:
+            print(f"No data for {date}, skipping...")
+            return None
+
         response.raise_for_status()
         return response.json()
     except requests.RequestException as e:
-        print(f"Error for the date {date}: {e}")
-        return None
+        print(e)
+        raise e
 
 
 def save_to_csv(data, filename):
     with open(filename, mode='w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
-        writer.writerow(["Draw date", "Lotto numbers"])
+        writer.writerow(["Draw date", "Lotto numbers", "Plus numbers"])
         
         for draw in data:
             writer.writerow(draw)
@@ -39,24 +49,34 @@ def save_to_csv(data, filename):
 
 if __name__ == '__main__':
     results = []
-    start_date = '2025-01-30'
+    date = datetime(2000, 1, 1)
+    end_date = datetime.today()
 
-    # for _ in range(?):
-    data = fetch_draw_results(start_date)
+    load_dotenv()
+    api_key = os.getenv('LOTTO_API_KEY')
 
-    if data and data.get('items'):
-        draw = data['items'][0]
-        draw_date = draw['drawDate']
-        numbers = draw['results'][0]['resultsJson']
-        
-        results.append([draw_date, ', '.join(map(str, numbers))])
-        print(f'Numbers: {draw_date} -> {numbers}')
+    while date <= end_date:
+        date_str = date.strftime('%Y-%m-%d')
 
-        start_date = draw_date[:10]
+        try:
+            data = fetch_draw_results(date_str, api_key)
+        except:
+            break
+
+        if data and data.get('items'):
+            items = data['items']
+            numbers = items[0]['results'][0]['resultsJson']
+            plus_numbers = items[1]['results'][0]['resultsJson'] if len(items) > 1 else []
+            
+            results.append([
+                date_str,
+                ','.join(map(str, numbers)),
+                ','.join(map(str, plus_numbers))
+            ])
+            print(f"Numbers: {date_str} -> {numbers}, (plus: {plus_numbers})")
+
+        date += timedelta(days=1)
         time.sleep(1)
-    else:
-        print(f'No data for {start_date}.')
-        # break
 
     save_to_csv(results, OUTPUT_FILE)
-    print(f'Data saved to file: {OUTPUT_FILE}')
+    print(f"Data saved to '{OUTPUT_FILE}'")
