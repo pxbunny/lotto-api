79e18c0e4e0604b2390d81a8aef9b5b323830b71
dae-ne <rafalcz29@gmail.com>
2025-02-02 18:35:01 +0100

    Move draw date to a separate column in the storage

--- tools/csv_to_storage.py
+++ tools/csv_to_storage.py
@@ -1,6 +1,7 @@
 import csv
 import os
 import uuid
+from datetime import datetime
 
 from dotenv import load_dotenv
 from azure.data.tables import TableServiceClient, TableTransactionError, UpdateMode
@@ -28,12 +29,17 @@ def upload_data_to_azure(csv_file, batch_size=100):
     batch = []
 
     with open(csv_file, mode='r', encoding='utf-8') as file:
+        MAX_DATE = datetime(9999, 12, 31)
         reader = csv.DictReader(file)
 
         for row in reader:
+            draw_date_str = row['Draw date']
+            draw_date = datetime.strptime(draw_date_str, "%Y-%m-%d")
+            reversed_draw_date = datetime(1, 1, 1) + (MAX_DATE - draw_date)
             entity = {
                 'PartitionKey': 'LottoData',
-                'RowKey': row['Draw date'],
+                'RowKey': reversed_draw_date.strftime("%Y-%m-%d").replace('-', ''),
+                'DrawDate': draw_date_str,
                 'LottoNumbers': row['Lotto numbers'],
                 'PlusNumbers': row['Plus numbers']
             }
