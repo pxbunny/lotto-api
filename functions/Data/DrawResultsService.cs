7294fea1513380fc9e2e49490d83e7c99f69b0b5
dae-ne <rafalcz29@gmail.com>
2025-02-10 23:52:32 +0100

    Update storage service

--- functions/Data/DrawResultsService.cs
+++ functions/Data/DrawResultsService.cs
@@ -16,8 +16,8 @@ sealed class DrawResultsService(TableServiceClient tableServiceClient)
         var drawDate = DateTime.Parse(data.DrawDate);
         var dateDifference = DateTime.MaxValue - drawDate;
         var reversedDrawDate = DateTime.MinValue + dateDifference;
-        var rowKey = reversedDrawDate.ToString(Constants.DateFormat).Replace("-", "");
-        
+        var rowKey = reversedDrawDate.ToString(Constants.DateFormat.Replace("-", ""));
+
         var entity = new DrawResultsEntity
         {
             PartitionKey = PartitionKey,
@@ -26,38 +26,33 @@ sealed class DrawResultsService(TableServiceClient tableServiceClient)
             LottoNumbers = data.LottoNumbersString,
             PlusNumbers = data.PlusNumbersString
         };
-        
+
         await _client.AddEntityAsync(entity, cancellationToken);
     }
 
     public async Task<DrawResultsEntity> GetLatestAsync(CancellationToken cancellationToken)
     {
-        return (await GetAsync(1, cancellationToken)).FirstOrDefault()
+        return (await GetAsync("", 1, cancellationToken)).FirstOrDefault()
                ?? throw new InvalidOperationException("Couldn't retrieve the latest draw results.");
-    }
-    
-    public async Task<IEnumerable<DrawResultsEntity>> GetAsync(int top, CancellationToken cancellationToken)
-    {
-        return await GetAsync("", top, cancellationToken);
     }
 
     public async Task<IEnumerable<DrawResultsEntity>> GetAsync(string filter, int top, CancellationToken cancellationToken)
     {
+        var results = new List<DrawResultsEntity>();
+
         var fullFilter = !string.IsNullOrWhiteSpace(filter) ? $"{BaseFilter} and ({filter})" : BaseFilter;
         var query = _client.QueryAsync<DrawResultsEntity>(fullFilter, cancellationToken: cancellationToken);
-        var pageSize = top > MaxPageSize ? MaxPageSize : top;
-        
-        var results = new List<DrawResultsEntity>();
+        var pageSize = Math.Min(MaxPageSize, top);
 
         await foreach (var page in query.AsPages(pageSizeHint: pageSize).WithCancellation(cancellationToken))
         {
             var remaining = top - results.Count;
-            var values = remaining < page.Values.Count
-                ? page.Values.Take(remaining)
-                : page.Values;
-            
-            results.AddRange(values);
-            
+
+            results.AddRange(
+                remaining < page.Values.Count
+                    ? page.Values.Take(remaining)
+                    : page.Values);
+
             if (results.Count >= top) break;
         }
 
