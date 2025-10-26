5eff68191fe996326fcee83df4c5791d023d59db
dae-ne <rafalcz29@gmail.com>
2025-10-26 20:23:24 +0100

    Add more infrastructure tests

--- tests/InfrastructureTests/FeatureInstallersTests.cs
+++ tests/InfrastructureTests/FeatureInstallersTests.cs
@@ -2,6 +2,8 @@
 
 public sealed class FeatureInstallersTests
 {
+    const string FeaturesNamespace = "Lotto.Features";
+
     [Fact]
     void MainInstaller_Should_FindFeatureInstallers()
     {
@@ -13,9 +15,32 @@ public sealed class FeatureInstallersTests
     void AllFeatureInstallers_Should_HavePublicParameterlessConstructor()
     {
         var installerTypes = GetFeatureInstallerTypes().ToList();
-
         Assert.NotEmpty(installerTypes);
         Assert.All(installerTypes, t => Assert.NotNull(t.GetConstructor(Type.EmptyTypes)));
+    }
+
+    [Fact]
+    void AllFeatureInstallers_Should_HaveCorrectNamespace()
+    {
+        var installerTypes = GetFeatureInstallerTypes().ToList();
+        Assert.All(installerTypes, t => Assert.StartsWith($"{FeaturesNamespace}.", t.Namespace));
+    }
+
+    [Fact]
+    void AllFeatureInstallers_Should_HaveExpectedNamespaceDepthLevel()
+    {
+        const int expectedNamespaceDepthLevel = 3;
+        var installerTypes = GetFeatureInstallerTypes().ToList();
+        var namespaceParts = installerTypes.Select(t => t.Namespace?.Split('.')).ToList();
+        Assert.All(namespaceParts, t => Assert.Equal(t?.Length, expectedNamespaceDepthLevel));
+    }
+
+    [Fact]
+    void EachFeature_Should_HaveOneInstaller()
+    {
+        var installerTypes = GetFeatureInstallerTypes().ToList();
+        var namespaces = installerTypes.Select(t => t.Namespace).ToList();
+        Assert.Equal(namespaces.Count, namespaces.Distinct().Count());
     }
 
     static IEnumerable<Type> GetFeatureInstallerTypes()
---
+++ tests/InfrastructureTests/FeatureTests.cs
@@ -0,0 +1,24 @@
+﻿namespace Lotto.InfrastructureTests;
+
+public sealed class FeatureTests
+{
+    const string FeaturesNamespace = "Lotto.Features";
+
+    [Fact]
+    void FeatureTypes_ShouldNot_BePlacedInTheRootFeatureNamespace()
+    {
+        var featureTypes = GetFeatureTypes();
+        var rootFeatreNamespaceDepth = FeaturesNamespace.Split(".").Length;
+        Assert.All(featureTypes, t => Assert.True(t.Namespace!.Split(".").Length > rootFeatreNamespaceDepth));
+    }
+
+    static IEnumerable<Type> GetFeatureTypes()
+    {
+        var assembly = typeof(IAssemblyMarker).Assembly;
+        return assembly.DefinedTypes.Where(t =>
+            t.Namespace is not null && t.Namespace.StartsWith($"{FeaturesNamespace}."));
+    }
+
+    static IEnumerable<IGrouping<string, Type>> GetFeatureTypesGroupedByFeatureName() =>
+        GetFeatureTypes().GroupBy(t => t.Namespace!.Split(".")[2]);
+}
