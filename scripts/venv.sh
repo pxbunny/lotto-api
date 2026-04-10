d91009aedc6d923258d0d0b920abfe79aeb02028
rczajkadev <rafalcz29@gmail.com>
2026-04-10 14:17:03 +0200

    Fix paths in scripts and git hooks

--- scripts/venv.sh
+++ scripts/venv.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 set_venv_python() {
-  root_dir="$1"
-  venv_python="$root_dir/.venv/Scripts/python.exe"
+  root_directory="$1"
+  venv_python="$root_directory/.venv/Scripts/python.exe"
 
   if [ ! -f "$venv_python" ]; then
-    venv_python="$root_dir/.venv/bin/python"
+    venv_python="$root_directory/.venv/bin/python"
   fi
 
   if [ ! -f "$venv_python" ]; then
-    echo "Python venv not found. Create it in $root_dir/.venv first." >&2
+    echo "Python venv not found. Create it in $root_directory/.venv first." >&2
     return 1
   fi
 
